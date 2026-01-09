d61082fbcabf8a054a6ecabd0b22ddaabebd5b76
Nurzamann <nurzaman0001@gmail.com>
2026-01-10 00:02:17 +0700

    my django project upload

---
+++ myweb/webku/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse    
+from django.template import loader
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+def login(request):
+    return render(request, 'login.html')
